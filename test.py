82c23e7e9290b4f6a9c672dd0f5d76638e01a4b8
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-04-10 13:08:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,87 @@
-import os
-# import subprocess
+import subprocess
 from time import sleep
+import time
+from subprocess import call
 
-# sensor = "ros2 topic echo /hadabot/wheel_radps_left"
 
-# output = subprocess.check_output(
-#     "ros2 topic echo /hadabot/wheel_radps_left", shell=True)
+# go forward
+motor_forward = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 1}'",
+    shell=True)
 
-# print(output)
+sleep(10)
 
-os.system(
-    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 1}'")
+# stop moving
+motor_stop = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 0}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 0}'",
+    shell=True)
+
 sleep(3)
-os.system(
-    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 0}'")
+
+# # go backwards
+motor_backwards = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: -1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: -1}'",
+    shell=True)
+
+sleep(10)
+
+# go right
+motor_right = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 0}'",
+    shell=True)
+
+sleep(10)
+
+
+# stop moving
+motor_stop = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 0}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 0}'",
+    shell=True)
+
+sleep(10)
+
+# go forward
+motor_forward = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 1}'",
+    shell=True)
+
+sleep(10)
+
+# go right
+motor_right = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 0}'",
+    shell=True)
+
+sleep(1)
+
+# go left
+motor_left = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: 0}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: 1}'",
+    shell=True)
+
+sleep(1)
+
+# # go backwards
+motor_backwards = subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_left std_msgs/msg/Float32 '{data: -1}'",
+    shell=True), subprocess.Popen(
+    "ros2 topic pub -1 /hadabot/wheel_power_right std_msgs/msg/Float32 '{data: -1}'",
+    shell=True)
+
+sleep(10)
